8946f1afcba42f60b5b16bf57985f4b8789da707
Thomas Ecalle <thomasecalle@gmail.com>
2023-05-02 10:40:58 +0200

    Add exercise 3 correction

--- lib/stateful_exercises/three.dart
+++ lib/stateful_exercises/three.dart
@@ -4,29 +4,54 @@ void main() {
   runApp(const ExerciseThree());
 }
 
-class ExerciseThree extends StatelessWidget {
+class ExerciseThree extends StatefulWidget {
   const ExerciseThree({Key? key}) : super(key: key);
+
+  @override
+  State<ExerciseThree> createState() => _ExerciseThreeState();
+}
+
+class _ExerciseThreeState extends State<ExerciseThree> {
+  Color _backgroundColor = Colors.amber;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amber,
+        backgroundColor: _backgroundColor,
         body: Center(
-          child: MySquare(),
+          child: MySquare(
+            onTap: _changeBackgroundColor,
+          ),
         ),
       ),
     );
   }
+
+  void _changeBackgroundColor() {
+    setState(() {
+      _backgroundColor = _backgroundColor == Colors.amber ? Colors.blue : Colors.amber;
+    });
+  }
 }
 
 class MySquare extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const MySquare({
+    Key? key,
+    required this.onTap,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 100,
-      color: Colors.white,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 100,
+        width: 100,
+        color: Colors.white,
+      ),
     );
   }
 }
